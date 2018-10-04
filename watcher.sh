2f32cc04e438e483fcdd999e279cf73ede760a8c
xanlab <alex.ahlstrom@hallmarklabs.com>
2018-10-04 13:56:00 -0700

    fixed conditionals

--- watcher.sh
+++ watcher.sh
@@ -1,11 +1,11 @@
 while true; do
 
 http_pid=`ps -ef |grep SimpleHTTPServer |grep 80 |awk '{print $2}'`
-status=`/bin/systemctl status mysqlrouter.service |grep running`
+status=`/bin/systemctl status mysqlrouter.service`
 
  if [[ ($status =~ "running") && (! $http_pid) ]]; then
-   python -m SimpleHTTPServer 80 &
- elif [[ ($status =~ "inactive") && ($http_pid) ]]; then
+   nohup python -m SimpleHTTPServer 80 &
+ elif [[ ($status =~ "inactive") && ($http_pid != "") ]]; then
    http_pid=`ps -ef |grep SimpleHTTPServer |grep 80 |awk '{print $2}'`
    kill -9 "$http_pid"
    echo "killing pid"
